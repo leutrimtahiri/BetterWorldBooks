c9eda420cc16388798908dc382c2a5abf16fad6a
Leutrim Tahiri <leutrimtahiri@yahoo.com>
2022-09-10 18:03:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
+import {useState} from "react"
+import Search from "./Components/Search";
+import {BrowserRouter as Router, Routes, Route }from "react-router-dom"
+import Children from "./Components/Children";
+import History from "./Components/History";
+import Home from "./Components/Home";
+import Top from "./Components/Top";
+import Basket from "./Components/Basket";
+import "./App.css"
+
+
 
 function App() {
+  const {products}=data;
+const [cartItems,setCartItems]=useState([])
+const onAdd=(product)=>{
+  const exist=cartItems.find(x=>x.id===product.id)
+  if(exist){
+    setCartItems(cartItems.map(x=>x.id===product.id?{...exist, qty:exist.qty+1}:x))
+  }
+  else{setCartItems([...cartItems,{...product,qty:1}])}
+}  
+const onRemove =(product)=>{
+  const exist=cartItems.find((x)=>x.id===product.id)
+  if(exist.qty===1){
+    setCartItems(cartItems.filter((x)=>x.id!==product.id))
+  }else{setCartItems(cartItems.map((x)=>x.id===product.id?{...exist, qty:exist.qty- 1}:x))
+
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        
+      <div id="Top"><Top countCartItems={cartItems.length}/></div>
+      <div id="basket"><Basket onAdd={onAdd} cartItems={cartItems} onRemove={onRemove}/></div>
+      <div id="Routes"> 
+      <Routes>
+        
+      <Route path="" element={<Home onAdd={onAdd} products={products}/>}/>
+     <Route path="/search" element={<Search onAdd={onAdd} products={products}/>}/>
+     
+     <Route path="/children" element={<Children  onAdd={onAdd} products={products} />}/>
+     <Route path="/history" element={<History  onAdd={onAdd} products={products}/>}/>
+     
+     </Routes>
+     
+     </div>
+     
+     
+     </Router>
+     
     </div>
   );
 }
